0df7261d8088b1b4b0d507bc4a4994f480325542
oceanebenkemoun <o.benkemoun@ileps.fr>
2025-11-24 13:45:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 document.getElementById("decouvrirBtn").addEventListener("click", function() {
   document.getElementById("styles").scrollIntoView({ behavior: "smooth" });
 });
+
+
+// --- SCRIPT POUR LES DESCRIPTIONS DES STYLES ---
+const styles = document.querySelectorAll('.style');
+const descriptionBox = document.getElementById('description');
+
+styles.forEach(style => {
+  style.addEventListener('click', () => {
+    const text = style.getAttribute('data-description');
+    descriptionBox.textContent = text;
+  });
+});
